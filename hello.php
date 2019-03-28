1b9359e7cfa54cfb49d5972dba426ddeb37eb0c2
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-03-28 17:50:13 +0300

    first php file

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+ echo "hello world";
+ // this is a single line comment
+ /*
+ this a multiline comment
+ it covers more than 1 line
+ */
+// variables
+ $mycounter = 1;
+ $mystring = "hello there";
+ $myarray = array("One", "Two", "Three");
+
+?>
