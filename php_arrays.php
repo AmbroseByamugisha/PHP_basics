af8722f291a538f8f93070403602862a65254764
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-03-29 19:35:19 +0300

    PHP arrays update 1

--- php_arrays.php
+++ php_arrays.php
@@ -7,4 +7,32 @@ $books[] = "fukunam";
 $books[] = "lost";
 
 print_r($books);
+
+echo "<br>";
+// the indexes can be words
+
+$places["Uganda"] = "Kampala";
+$places["Germany"] = "Berlin";
+$places["China"] = "Beijing";
+
+print_r($places);
+
+echo "<br>";
+// other assignment techniques
+$places = array(
+"Uganda" => "Kampala",
+"Germany" => "Berlin",
+"Kenya" => "Nairobi");
+echo $places["Uganda"];
+
+echo "<br>";
+// using the foreach loop
+$colours = array("blue", "red", "yellow");
+$i = 0;
+
+foreach($colours as $item)
+{
+	echo "$i: $item<br>";
+	++$i;
+} 
 ?>
