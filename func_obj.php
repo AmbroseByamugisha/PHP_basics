a34bc4c25ae04325f2494682a9a5634b0944de67
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-03-29 17:42:56 +0300

    Functions and objects - second topic update

--- func_obj.php
+++ func_obj.php
@@ -23,5 +23,37 @@ function do_that($c1, $c2, $c3)
 
 	return array($c1, $c2, $c3);
 }
+echo "<br>";
+echo function_exists("do_that");
+// CLASS
+$object = new User;
+print_r($object); echo "<br>";
+
+$object -> name = "Joe";
+$object -> password = "pass123";
+print_r($object); echo "<br>";
+
+$object-> save_user();
+
+class User
+{
+	public $name, $password;
+
+	function save_user()
+	{
+		echo "Save User code goes here";
+	}
+}
+
+echo "<br>";
+
+$object1 = new User();
+$object1 -> name = "Athur";
+$object2 = clone $object1;
+$object2 -> name = "Zari";
+
+echo "object1 name = " .$object1 -> name . "<br>";
+echo "object2 name = " .$object2 -> name;
+
 
 ?>
