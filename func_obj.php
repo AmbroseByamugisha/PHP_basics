387e13d40769b7ebd93f1766bc224fb4a3445350
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-03-29 19:04:54 +0300

    Functions and objects - second topic update 2

--- func_obj.php
+++ func_obj.php
@@ -55,5 +55,45 @@ $object2 -> name = "Zari";
 echo "object1 name = " .$object1 -> name . "<br>";
 echo "object2 name = " .$object2 -> name;
 
+echo "<br>";
+// creating and accessing a static method
+User_1 :: pwd_string();
+
+class User_1
+{
+	static function pwd_string()
+	{
+		echo "Please enter your password";
+	}
+} 
+echo "<br>";
+$object = new Subscriber_1;
+$object -> name = "Byamu";
+$object -> password = "pword";
+$object -> phone = "0779756164";
+$object -> email = "byamu@gmail.com";
+$object -> display();
+
+class User_2
+{
+	public $name, $password;
+	function save_user()
+	{
+		echo "Save user code goes here";
+	}
+}
+class Subscriber_1 extends User_2
+{
+	public $phone, $email;
+
+	function display()
+	{
+		echo "Name: " . $this -> name . "<br>";
+		echo "Pass: " . $this -> password . "<br>";
+		echo "Phone: " . $this -> phone. "<br>";
+		echo "Email: " . $this -> email;
+	}
+}
+
 
 ?>
