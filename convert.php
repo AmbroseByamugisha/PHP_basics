1082298c374c9bdea2b93d2adb9896925b4283f6
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-04-03 12:58:53 +0300

    another form example

---
+++ convert.php
@@ -0,0 +1,45 @@
+<?php // convert.php
+$f = $c = '';
+
+if (isset($_POST['f'])) $f = sanitizeString($_POST['f']);
+if (isset($_POST['f'])) $f = sanitizeString($_POST['f']);
+
+if ($f != '')
+{
+	$c = intval((5/9) * ($f - 32));
+	$out = "$f f equals $c c";
+}
+else if($c != '')
+{
+	$f = intval((9/5) * $c + 32);
+	$out = "$c c equals $f f";
+}
+else $out = "";
+
+echo <<<_END
+<html>
+<head>
+</head>
+<body>
+<pre>
+Enter either Fahrenheit or Celsius and click on Convert
+
+<b>$out</b>
+<form method="post" action="convert.php">
+Fahrenheit <input type="text" name="f" size="7">
+Celsius <input type="text" name="c" size="7">
+<input type="submit" value="Convert">
+</form>
+</pre>
+</body>
+</html>
+_END;
+
+function sanitizeString($var)
+{
+	$var = stripslashes($var);
+	$var = strip_tags($var);
+	$var = htmlentities($var);
+	return $var;
+}
+?>
